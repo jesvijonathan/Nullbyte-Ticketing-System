12d3b69d74235dd45d15a9ad6ab1903bbaccb0f6
HaythmKenway <akileswar2002@protonmail.com>
2024-10-13 08:37:08 +0530

    dont return Null for jwt

--- app/Backend/modules/auth/auth.py
+++ app/Backend/modules/auth/auth.py
@@ -67,12 +67,6 @@ def cleanup_user(token=None, user_id=None):
 def jwt_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        token = None
-
-        
-        # bypass jwt check if testing
-        return f(*args, **kwargs)
-
         if 'Authorization' in request.headers:
             token = request.headers['Authorization'].split(" ")[1]
         elif 'session' in request.cookies:
